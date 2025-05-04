e61998667f3c7b09cd8078fb757dbe59f2f87904
Nadia khaled <d703403@gmail.com>
2025-05-05 02:39:06 +0300

    add note model to hive box

--- lib/cubits/add_note_cubit/add_note_cubit.dart
+++ lib/cubits/add_note_cubit/add_note_cubit.dart
@@ -1,8 +1,22 @@
 import 'package:bloc/bloc.dart';
+import 'package:hive/hive.dart';
 import 'package:meta/meta.dart';
+import 'package:notes_app/constant.dart';
+import 'package:notes_app/models/note_model.dart';
 
 part 'add_note_cubit_state.dart';
 
 class AddNoteCubit extends Cubit<AddNoteCubitState> {
   AddNoteCubit() : super(AddNoteInitialState());
+
+   addNote(NoteModel note)async {
+    emit(AddNoteLoadingState());
+try{
+var notesBox= Hive.box<NoteModel>(kNotesBox);
+emit( AddNoteSuccessState());
+await notesBox.add( note);
+}catch(e){
+emit( AddNoteFailureState(errorMessage: e.toString()));
+}
+  }
 }
